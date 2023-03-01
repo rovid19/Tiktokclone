36e753c2648331af0352e36066b80f56795bdb7e
rovid19 <118137968+rovid19@users.noreply.github.com>
2023-03-01 02:39:59 +0100

    final update
    final change for cors so it can run

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGOOSE_URL);
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000", "https://gymtok.onrender.com"],
+    origin: ["http://localhost:3000", "https://gymtokweb.onrender.com"],
   })
 );
 app.use(express.json());
